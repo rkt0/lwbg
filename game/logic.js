5102495b9d45372b7403a6f14e83021ef87d5a42
rkt0 <raymond.kyle.thomas.0@gmail.com>
2026-04-20 19:52:29 -0400

    refactor game logic data

--- game/logic.js
+++ game/logic.js
@@ -1,54 +1,16 @@
-// Container object for board with basic information
 export const bd = {
   nHumanSpaces: 109,
   nHumanPieces: 12,
-  humanStart: 0, humanGoal: 107, humanDead: 108,
+  humanStart: 0,
+  humanGoal: 107,
+  humanDead: 108,
+  humanEdges: [],
+  humanJumps: [[4, 86], [20, 29]],
   trexStart: 11,
   nRaptorSpaces: 57,
   nRaptorPieces: 4,
   raptorStart: [47, 49, 52, 53],
-  firstTurn: 'human',
-};
-
-// Human edges and jumps
-{
-  const humanPaths = [
-    [0, 1, 2, 3, 4],
-    [1, 5, 6, 7, 8, 4],
-    [5, 9, 10, 11, 12, 24],
-    [9, 13, 14, 15, 16, 17, 18, 19, 20],
-    [19, 21, 22, 23, 24],
-    [23, 25, 26, 27, 28, 29],
-    [27, 30, 31, 32, 33, 61],
-    [33, 34, 35, 36, 37, 38, 107],
-    [14, 39, 40, 41, 42, 43, 44, 20],
-    [19, 45, 46, 47, 48, 49, 29],
-    [49, 50, 51, 52, 53, 54, 55, 61],
-    [55, 56, 57, 58, 59, 60, 107],
-    [7, 62, 63, 64, 65, 66, 67, 24],
-    [66, 68, 69, 70, 71, 72],
-    [26, 72, 73, 74, 75, 76, 77, 78, 79, 107],
-    [25, 71],
-    [4, 80, 81, 82, 83, 84, 85, 86, 64],
-    [85, 87, 88, 89, 90, 91, 92, 93, 70],
-    [92, 94, 95, 96, 97, 98, 99, 100, 106, 76],
-    [100, 101, 102, 103, 104, 105, 78],
-  ];
-  bd.humanEdges = [];
-  for (const path of humanPaths) {
-    for (let i = 1; i < path.length; i++) {
-      bd.humanEdges.push([path[i - 1], path[i]]);
-    }
-  }
-  bd.humanJumps = [
-    [ 4, 86],
-    [20, 29],
-  ];
-}
-
-// Raptor edges and entrances
-{
-  bd.raptorEdges = [
+  raptorEdges: [
     [0, 1], [0, 3],
     [1, 7],
     [3, 4],
@@ -93,22 +55,12 @@ export const bd = {
     [51, 52],
     [52, 53],
     [53, 54],
-  ];
-  bd.raptorEntrances = [
-    [ 3,  2],
-    [ 8,  6],
-    [12,  9],
-    [14, 13],
-    [22, 18],
-    [37, 28],
-    [39, 32],
-    [48, 40],
-  ];
-}
-
-// Relationship between human and raptor spaces
-{
-  bd.humanSpacesInRaptorSpace = [
+  ],
+  raptorEntrances: [
+    [3, 2], [8, 6], [12, 9], [14, 13],
+    [22, 18], [37, 28], [39, 32], [48, 40],
+  ],
+  humanSpacesInRaptorSpace: [
     [],                   //  0
     [80, 81],
     [4],
@@ -166,55 +118,77 @@ export const bd = {
     [57, 58],
     [107],                // 55
     [0],
-  ];
-}
+  ],
+  bldgHumanSpaces: [
+    4, 86, 93, 106, 14, 20, 29, 61, 24, 107, 0,
+  ],
+  firstTurn: 'human',
+  humanChoices: {move: [], jump: []},
+  raptorChoices: {move: [], enter: []},
+};
 
-// Buildings
-{
-  bd.bldgHumanSpaces = [
-    4, 86, 93, 106, 14, 20, 29, 61, 24, 107, 0
-  ];
-  bd.bldgRaptorSpaces = bd.bldgHumanSpaces.map(
-    hSpace => bd.humanSpacesInRaptorSpace.findIndex(
-      hSpacesIn => hSpacesIn.includes(hSpace)
-    )
-  );
-}
-
-// Move and jump/enter choices
-{
-  bd.humanChoices = {move: [], jump: []};
-  for (let s = 0; s < bd.nHumanSpaces; s++) {
-    const mChoices = new Set();
-    const jChoices = new Set();
-    for (const edge of bd.humanEdges) {
-      if (edge[0] === s) mChoices.add(edge[1]);
-      if (edge[1] === s) mChoices.add(edge[0]);
-    }
-    for (const jump of bd.humanJumps) {
-      if (jump[0] === s) jChoices.add(jump[1]);
-    }
-    bd.humanChoices.move.push(mChoices);
-    bd.humanChoices.jump.push(jChoices);
-  }
-  bd.raptorChoices = {move: [], enter: []};
-  for (let s = 0; s < bd.nRaptorSpaces; s++) {
-    const mChoices = new Set();
-    const eChoices = new Set();
-    for (const edge of bd.raptorEdges) {
-      if (edge[0] === s) mChoices.add(edge[1]);
-      if (edge[1] === s) mChoices.add(edge[0]);
-    }
-    for (const enter of bd.raptorEntrances) {
-      if (enter[0] === s) eChoices.add(enter[1]);
-      if (enter[1] === s) mChoices.add(enter[0]);
-    }
-    bd.raptorChoices.move.push(mChoices);
-    bd.raptorChoices.enter.push(eChoices);
+const humanPaths = [
+  [0, 1, 2, 3, 4],
+  [1, 5, 6, 7, 8, 4],
+  [5, 9, 10, 11, 12, 24],
+  [9, 13, 14, 15, 16, 17, 18, 19, 20],
+  [19, 21, 22, 23, 24],
+  [23, 25, 26, 27, 28, 29],
+  [27, 30, 31, 32, 33, 61],
+  [33, 34, 35, 36, 37, 38, 107],
+  [14, 39, 40, 41, 42, 43, 44, 20],
+  [19, 45, 46, 47, 48, 49, 29],
+  [49, 50, 51, 52, 53, 54, 55, 61],
+  [55, 56, 57, 58, 59, 60, 107],
+  [7, 62, 63, 64, 65, 66, 67, 24],
+  [66, 68, 69, 70, 71, 72],
+  [26, 72, 73, 74, 75, 76, 77, 78, 79, 107],
+  [25, 71],
+  [4, 80, 81, 82, 83, 84, 85, 86, 64],
+  [85, 87, 88, 89, 90, 91, 92, 93, 70],
+  [92, 94, 95, 96, 97, 98, 99, 100, 106, 76],
+  [100, 101, 102, 103, 104, 105, 78],
+];
+for (const path of humanPaths) {
+  for (let i = 1; i < path.length; i++) {
+    bd.humanEdges.push([path[i - 1], path[i]]);
   }
 }
 
-// Separate object for dice
+bd.bldgRaptorSpaces = bd.bldgHumanSpaces.map(
+  hSpace => bd.humanSpacesInRaptorSpace.findIndex(
+    hSpacesIn => hSpacesIn.includes(hSpace)
+  )
+);
+
+for (let s = 0; s < bd.nHumanSpaces; s++) {
+  const mChoices = new Set();
+  const jChoices = new Set();
+  for (const edge of bd.humanEdges) {
+    if (edge[0] === s) mChoices.add(edge[1]);
+    if (edge[1] === s) mChoices.add(edge[0]);
+  }
+  for (const jump of bd.humanJumps) {
+    if (jump[0] === s) jChoices.add(jump[1]);
+  }
+  bd.humanChoices.move.push(mChoices);
+  bd.humanChoices.jump.push(jChoices);
+}
+for (let s = 0; s < bd.nRaptorSpaces; s++) {
+  const mChoices = new Set();
+  const eChoices = new Set();
+  for (const edge of bd.raptorEdges) {
+    if (edge[0] === s) mChoices.add(edge[1]);
+    if (edge[1] === s) mChoices.add(edge[0]);
+  }
+  for (const enter of bd.raptorEntrances) {
+    if (enter[0] === s) eChoices.add(enter[1]);
+    if (enter[1] === s) mChoices.add(enter[0]);
+  }
+  bd.raptorChoices.move.push(mChoices);
+  bd.raptorChoices.enter.push(eChoices);
+}
+
 export const dice = {
   human: {
     movement: ['Jump', 2, 3, 3, 4, 4],
@@ -228,11 +202,3 @@ export const dice = {
     movement: [0, 0, 0, 1, 1, 1],
   },
 };
-
-// Detect if script is running in simulation mode
-//   (i.e., in node rather than in a broswer)
-// If so, assign settings needed for simulation
-//   to the global object
-if (typeof global !== typeof void 0) {
-  Object.assign(global, {bd, dice});
-}
