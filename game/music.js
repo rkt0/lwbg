00f2c0c2bceda1391d26537ebec0079a5ca9d6bb
rkt0 <raymond.kyle.thomas.0@gmail.com>
2026-04-20 21:02:16 -0400

    refactor audio modules

--- game/music.js
+++ game/music.js
@@ -1,104 +1,103 @@
-import {qs, ael} from './utility.js';
+import {qs, ael, isNull} from './utility.js';
 import {debug} from './debug.js';
 import {prng} from './prngs.js';
 
+class Track {
+  constructor(title, quality, heavy) {
+    this.src = `audio/music/${
+      title.toLowerCase().replaceAll(' ', '-')
+    }.mp3`;
+    this.title = title;
+    this.freqWeight = Math.exp(-0.5 * quality);
+    this.heavy = heavy;
+  }
+}
+
 export const music = {
   element: qs('#music-player'),
-  dir: 'audio/music',
   tooRecent: 6,
   recentIds: [],
   nowPlaying: null,
   audioOn: true,
-  freqWeightFn: x => Math.exp(- x / 2),
-}
-music.playlist = [
-  new Track('Grim Idol',            1, true),
-  new Track('Obliteration',         1, true),
-  new Track('Volatile Reaction',    1, true),
-  new Track('Feral Chase',          1, false),
-  new Track('Malicious',            1, false),
-  new Track('Hot Pursuit',          1, false),
-  new Track('Black Vortex',         2, true),
-  new Track('Eternal Terminal',     2, true),
-  new Track('Mistake the Getaway',  2, true),
-  new Track('Serpentine Trek',      2, false),
-  new Track('Shamanistic',          2, false),
-  new Track('Zombie Chase',         2, false),
-  new Track('Chase',                3, true),
-  new Track('Clenched Teeth',       3, true),
-  new Track('Darkling',             3, true),
-  new Track('Constance',            3, false),
-  new Track('Drums of the Deep',    3, false),
-  new Track('Prelude and Action',   3, false),
-  new Track('Exotic Battle',        4, true),
-  new Track('Faceoff',              4, true),
-  new Track('Nerves',               4, true),
-  new Track('Dangerous',            4, false),
-  new Track('Enter the Maze',       4, false),
-  new Track('River of Io',          4, false),
-];
-
-// Track constructor
-function Track(title, quality, heavy) {
-  this.src = `${music.dir}/${
-    title.toLowerCase().replaceAll(' ', '-')
-  }.mp3`;
-  this.title = title;
-  this.freqWeight = music.freqWeightFn(quality);
-  this.heavy = heavy;
+  playlist: [
+    new Track('Grim Idol', 1, true),
+    new Track('Obliteration', 1, true),
+    new Track('Volatile Reaction', 1, true),
+    new Track('Feral Chase', 1, false),
+    new Track('Malicious', 1, false),
+    new Track('Hot Pursuit', 1, false),
+    new Track('Black Vortex', 2, true),
+    new Track('Eternal Terminal', 2, true),
+    new Track('Mistake the Getaway', 2, true),
+    new Track('Serpentine Trek', 2, false),
+    new Track('Shamanistic', 2, false),
+    new Track('Zombie Chase', 2, false),
+    new Track('Chase', 3, true),
+    new Track('Clenched Teeth', 3, true),
+    new Track('Darkling', 3, true),
+    new Track('Constance', 3, false),
+    new Track('Drums of the Deep', 3, false),
+    new Track('Prelude and Action', 3, false),
+    new Track('Exotic Battle', 4, true),
+    new Track('Faceoff', 4, true),
+    new Track('Nerves', 4, true),
+    new Track('Dangerous', 4, false),
+    new Track('Enter the Maze', 4, false),
+    new Track('River of Io', 4, false),
+  ],
+  play(id) {
+    music.element.src = music.playlist[id].src;
+    music.element.play();
+    music.nowPlaying = id;
+  },
+  next(skipCurrent, any) {
+    const {
+      playlist: pl, recentIds: ri, nowPlaying: now,
+    } = music;
+    const {cycle, startAt} = debug.music;
+    if (cycle) {
+      if (isNull(now)) music.play(startAt);
+      else music.play((now + 1) % pl.length);
+      return;
+    }
+    if (!isNull(now) && ! skipCurrent) {
+      if (ri.length === music.tooRecent) ri.shift();
+      ri.push(now);
+    }
+    const avoid = [...ri];
+    if (skipCurrent) avoid.push(now);
+    let nextId, okNext;
+    while (!okNext) {
+      const rand = prng.music();
+      nextId = 0;
+      while (music.cdf[nextId] < rand) nextId++;
+      if (any && nextId !== now) break;
+      if (avoid.includes(nextId)) continue;
+      const half = Math.floor(music.tooRecent / 2);
+      const l = ri.length;
+      const nH = ri.filter((e) => pl[e].heavy).length;
+      const nextIsH = pl[nextId].heavy;
+      if (l >= half * 2) okNext = true;
+      else if (l) {
+        okNext = nH === half ? !nextIsH :
+          l - nH === half ? nextIsH : true;
+      } else okNext = !nextIsH;
+    }
+    music.play(nextId);
+  },
 }
 
 // Compute cdf of track sampling distribution
-{
-  const nT = music.playlist.length;
-  music.cdf = new Array(nT).fill(0);
-  for (let i = 0; i < nT; i++) {
-    for (let j = 0; j <= i; j++) {
-      music.cdf[i] += music.playlist[j].freqWeight;
-    }
+const nTracks = music.playlist.length;
+music.cdf = new Array(nTracks).fill(0);
+for (let i = 0; i < nTracks; i++) {
+  for (let j = 0; j <= i; j++) {
+    music.cdf[i] += music.playlist[j].freqWeight;
   }
-  const tot = music.cdf[nT - 1];
-  for (let i = 0; i < nT; i++) music.cdf[i] /= tot;
 }
-
-// Draw next track and play
-music.next = (skipCurrent, any) => {
-  let nextId, okNext;
-  const {playlist: pl, recentIds: ri} = music;
-  if (music.nowPlaying !== null && ! skipCurrent) {
-    if (ri.length === music.tooRecent) ri.shift();
-    ri.push(music.nowPlaying);
-  }
-  const {cycle, startAt} = debug.music;
-  if (cycle) {
-    okNext = true;
-    nextId = (music.nowPlaying ?? (startAt - 1)) + 1;
-    if (nextId === pl.length) nextId = 0;
-  }
-  const avoid = [...ri];
-  if (skipCurrent) avoid.push(music.nowPlaying);
-  while (! okNext) {
-    const rand = prng.music();
-    nextId = 0;
-    while (music.cdf[nextId] < rand) nextId++;
-    if (any && nextId !== music.nowPlaying) break;
-    if (avoid.includes(nextId)) continue;
-    const half = Math.floor(music.tooRecent / 2);
-    const l = ri.length;
-    const nH = ri.filter(e => pl[e].heavy).length;
-    const nextIsH = pl[nextId].heavy;
-    if (l >= half * 2) {
-      okNext = true;
-    } else if (l) {
-      okNext = nH === half ? ! nextIsH :
-          l - nH === half ? nextIsH : true;
-    } else {
-      okNext = ! nextIsH;
-    }
-  }
-  music.element.src = music.playlist[nextId].src;
-  music.element.play();
-  music.nowPlaying = nextId;
-};
+const totalWeight = music.cdf[nTracks - 1];
+for (let i = 0; i < nTracks; i++) {
+  music.cdf[i] /= totalWeight;
+}
 
 ael(music.element, 'ended', () => {music.next();});
--- game/sfx.js
+++ game/sfx.js
@@ -3,61 +3,61 @@ import {rollDie} from './utility.js';
 import {prng} from './prngs.js';
 import {music} from './music.js';
 
-export const sfx = {
-  element: qs('#sfx-player'),
-  dir: 'audio/sfx',
-};
-sfx.sound = [
-  new SoundEffect('trex-roar'),
-  new SoundEffect('trex-stomp'),
-  // Raptor sound freqWeight should be integer
-  new SoundEffect('raptor-0', 1),
-  new SoundEffect('raptor-1', 2),
-  new SoundEffect('raptor-2', 1),
-  new SoundEffect('raptor-3', 2),
-  new SoundEffect('raptor-4', 2),
-  new SoundEffect('raptor-5', 2),
-  new SoundEffect('raptor-6', 2),
-];
-
-// SoundEffect constructor
-function SoundEffect(title, freqWeight = 1) {
-  this.src = `${sfx.dir}/${title}.mp3`;
-  this.title = title;
-  this.freqWeight = freqWeight;
+class SoundEffect {
+  constructor(title, freqWeight = 1) {
+    this.src = `audio/sfx/${title}.mp3`;
+    this.title = title;
+    this.freqWeight = freqWeight;
+  }
 }
 
-// Play sounds
-sfx.play = title => {
-  if (!music.audioOn) return;
-  sfx.element.src =
-    sfx.sound.find(x => x.title === title).src;
-  sfx.element.play();
+export const sfx = {
+  element: qs('#sfx-player'),
+  sounds: [
+    new SoundEffect('trex-roar'),
+    new SoundEffect('trex-stomp'),
+    // Raptor sound freqWeight should be integer
+    new SoundEffect('raptor-0', 1),
+    new SoundEffect('raptor-1', 2),
+    new SoundEffect('raptor-2', 1),
+    new SoundEffect('raptor-3', 2),
+    new SoundEffect('raptor-4', 2),
+    new SoundEffect('raptor-5', 2),
+    new SoundEffect('raptor-6', 2),
+  ],
+  play(title) {
+    if (!music.audioOn) return;
+    sfx.element.src =
+      sfx.sounds.find(x => x.title === title).src;
+    sfx.element.play();
+  },
+  trexRoar() {
+    sfx.play('trex-roar');
+  },
+  trexStomp() {
+    sfx.play('trex-stomp');
+  },
+  raptor() {
+    if (sfx.element.src && ! sfx.element.ended) {
+      return;
+    }
+    const id = rollDie(sfx.raptorIdArr, prng.sfx);
+    sfx.play(`raptor-${id}`);
+    sfx.raptorAlreadyPlayed = true;
+  },
+  // To prevent raptor sounds from playing
+  // more than once on same die roll, this indicator
+  // is set to true when raptor sound is played
+  // and checked to prevent sound from playing again
+  raptorAlreadyPlayed: false,
 };
 
-// T-rex sounds
-sfx.trexRoar  = () => sfx.play('trex-roar');
-sfx.trexStomp = () => sfx.play('trex-stomp');
-
-// Raptor sounds
-sfx.raptorSounds = sfx.sound.filter(
+sfx.raptorSounds = sfx.sounds.filter(
   x => x.title.startsWith('raptor')
 );
-sfx.raptorIdArr = sfx.raptorSounds.map(x => {
+sfx.raptorIdArr = sfx.raptorSounds.map((x) => {
   const id = x.title.split('-')[1];
   const arr = [];
   for (let i = 0; i < x.freqWeight; i++) arr.push(id);
   return arr;
 }).flat();
-sfx.raptor = () => {
-  if (sfx.element.src && ! sfx.element.ended) return;
-  const soundId = rollDie(sfx.raptorIdArr, prng.sfx);
-  sfx.play(`raptor-${soundId}`);
-  sfx.raptorAlreadyPlayed = true;
-};
-
-// To prevent raptor sounds from playing
-//   more than once on same die roll, this indicator
-//   is set to true when raptor sound is played
-//   and checked to prevent sound from playing again
-sfx.raptorAlreadyPlayed = false;
