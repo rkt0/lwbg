34fa518f03f7184dc2ba41593657157059bd85a7
rkt0 <raymond.kyle.thomas.0@gmail.com>
2026-07-31 01:14:40 -0400

    improve hover effect for message and buttons

--- game/message.js
+++ game/message.js
@@ -5,7 +5,7 @@ import {anim} from './animation.js';
 
 export const message = {
   suppress: false,
-  show(templateId, append) {
+  async show(templateId, append) {
     if (this.suppress) return;
     const node = fromTemplate(templateId);
     if (append && element.dataset.appendable === '') {
@@ -13,17 +13,20 @@ export const message = {
     } else element.replaceChildren(node);
     if (append) element.dataset.appendable = '';
     element.classList.remove('being-hidden');
-    anim.slide(element, 1, anim.time.messageSlide);
+    await anim.slide(element, 1, aTime);
+    element.inert = false;
   },
   hide() {
     if (element.style.display === 'none') return;
     if (anim.isAnimated(element)) return;
+    element.inert = true;
     element.classList.add('being-hidden');
     delete element.dataset.appendable;
-    anim.slide(element, 0, anim.time.messageSlide);
+    anim.slide(element, 0, aTime);
   },
 };
 
 const element = qjs('gameplay-message');
+const aTime = anim.time.messageSlide;
 
 ael(element, 'mousedown', () => message.hide());
