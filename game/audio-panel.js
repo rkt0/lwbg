308095a12479d2014746eb7f0e3be64b8202bcd4
rkt0 <raymond.kyle.thomas.0@gmail.com>
2026-09-04 00:52:03 -0400

    add non-functional music track selection panel

--- game/audio-panel.js
+++ game/audio-panel.js
@@ -1,11 +1,12 @@
 import {qjs, closestData} from './utility.js';
 import {atClick} from './mouse-events.js';
+import {template} from './template.js';
 import {music} from './music.js';
 
 export const audioPanel = {
   show() {
     if (!music.allowed) return;
-    section.style.display = 'grid';
+    section.style.display = '';
     section.inert = false;
   },
   hide() {
@@ -16,14 +17,51 @@ export const audioPanel = {
 
 // Element references
 const section = qjs('audio-panel');
+const musicPanel = qjs('music-panel');
+const playlistPanel = qjs('playlist-panel');
+const playlist = qjs('playlist');
+
+// Create song list
+const tracks = [];
+for (const [id, track] of music.playlist.entries()) {
+  const element = template('track');
+  element.dataset.track = id;
+  element.textContent = track.title;
+  tracks.push(element);
+}
+playlist.append(...tracks);
+
+// Needed for click handler
+function showMusicPanel() {
+  musicPanel.style.display = '';
+  musicPanel.inert = false;
+}
+function hideMusicPanel() {
+  musicPanel.inert = true;
+  musicPanel.style.display = 'none';
+}
+function showPlaylist() {
+  hideMusicPanel();
+  playlistPanel.style.display = '';
+  playlistPanel.inert = false;
+}
+function hidePlaylist() {
+  showMusicPanel();
+  playlistPanel.inert = true;
+  playlistPanel.style.display = 'none';
+}
 
 // Dispatch table for click handler
 const dispatch = {
   'next-track': () => music.next(),
-  'hide-music': () => audioPanel.hide(),
+  'choose-track': showPlaylist,
+  'hide-audio': () => audioPanel.hide(),
+  'hide-playlist': hidePlaylist,
 };
 
 // Click handler
 atClick(section, (e) => {
+  const track = closestData(e, 'track');
+  if (track) return console.log(`play ${track}`);
   dispatch[closestData(e)]?.();;
 });
--- game/music.js
+++ game/music.js
@@ -2,6 +2,18 @@ import {qjs, qd, isNull} from './utility.js';
 import {prng} from './prngs.js';
 import {debug} from './debug.js';
 
+class Track {
+  constructor(title, quality, heavy) {
+    this.src = `audio/music/${
+      title.toLowerCase().replaceAll(' ', '-')
+    }.mp3`;
+    this.title = title;
+    this.freqWeight = Math.exp(-0.5 * quality);
+    this.heavy = heavy;
+    this.artist = 'Kevin MacLeod';
+  }
+}
+
 export const music = {
   element: qjs('music-player'),
   audioOn: false,
@@ -31,9 +43,9 @@ export const music = {
       const half = Math.floor(tooRecent / 2);
       const l = recentIds.length;
       const nHeavy = recentIds.filter((e) => {
-        return playlist[e].heavy
+        return this.playlist[e].heavy
       }).length;
-      const nextIsHeavy = playlist[nextId].heavy;
+      const nextIsHeavy = this.playlist[nextId].heavy;
       if (l >= half * 2) okNext = true;
       else if (l) {
         okNext = nHeavy === half ? !nextIsHeavy :
@@ -50,13 +62,39 @@ export const music = {
       else this.next();
     } else element.pause();
   },
+  playlist: [
+    new Track('Grim Idol', 1, true),
+    new Track('Obliteration', 1, true),
+    new Track('Volatile Reaction', 1, true),
+    new Track('Feral Chase', 1, false),
+    new Track('Malicious', 1, false),
+    new Track('Hot Pursuit', 1, false),
+    new Track('Black Vortex', 2, true),
+    new Track('Eternal Terminal', 2, true),
+    new Track('Mistake the Getaway', 2, true),
+    new Track('Serpentine Trek', 2, false),
+    new Track('Shamanistic', 2, false),
+    new Track('Zombie Chase', 2, false),
+    new Track('Chase', 3, true),
+    new Track('Clenched Teeth', 3, true),
+    new Track('Darkling', 3, true),
+    new Track('Constance', 3, false),
+    new Track('Drums of the Deep', 3, false),
+    new Track('Prelude and Action', 3, false),
+    new Track('Exotic Battle', 4, true),
+    new Track('Faceoff', 4, true),
+    new Track('Nerves', 4, true),
+    new Track('Dangerous', 4, false),
+    new Track('Enter the Maze', 4, false),
+    new Track('River of Io', 4, false),
+  ],
 };
 
 const tooRecent = 6;
 const recentIds = [];
 let nowPlaying = null;
 function play(id) {
-  const {src, title, artist} = playlist[id];
+  const {src, title, artist} = music.playlist[id];
   music.element.src = src;
   songElement.textContent = title;
   artistElement.textContent = artist;
@@ -64,49 +102,11 @@ function play(id) {
   if (music.audioOn) music.element.play();
 };
 
-class Track {
-  constructor(title, quality, heavy) {
-    this.src = `audio/music/${
-      title.toLowerCase().replaceAll(' ', '-')
-    }.mp3`;
-    this.title = title;
-    this.freqWeight = Math.exp(-0.5 * quality);
-    this.heavy = heavy;
-    this.artist = 'Kevin MacLeod';
-  }
-}
-const playlist = [
-  new Track('Grim Idol', 1, true),
-  new Track('Obliteration', 1, true),
-  new Track('Volatile Reaction', 1, true),
-  new Track('Feral Chase', 1, false),
-  new Track('Malicious', 1, false),
-  new Track('Hot Pursuit', 1, false),
-  new Track('Black Vortex', 2, true),
-  new Track('Eternal Terminal', 2, true),
-  new Track('Mistake the Getaway', 2, true),
-  new Track('Serpentine Trek', 2, false),
-  new Track('Shamanistic', 2, false),
-  new Track('Zombie Chase', 2, false),
-  new Track('Chase', 3, true),
-  new Track('Clenched Teeth', 3, true),
-  new Track('Darkling', 3, true),
-  new Track('Constance', 3, false),
-  new Track('Drums of the Deep', 3, false),
-  new Track('Prelude and Action', 3, false),
-  new Track('Exotic Battle', 4, true),
-  new Track('Faceoff', 4, true),
-  new Track('Nerves', 4, true),
-  new Track('Dangerous', 4, false),
-  new Track('Enter the Maze', 4, false),
-  new Track('River of Io', 4, false),
-];
-const nTracks = playlist.length;
-
+const nTracks = music.playlist.length;
 const cdf = Array(nTracks).fill(0);
-cdf[0] = playlist[0].freqWeight;
+cdf[0] = music.playlist[0].freqWeight;
 for (let i = 1; i < nTracks; i++) {
-  cdf[i] = cdf[i - 1] + playlist[i].freqWeight;
+  cdf[i] = cdf[i - 1] + music.playlist[i].freqWeight;
 }
 const total = cdf[nTracks - 1];
 for (let i = 0; i < nTracks; i++) cdf[i] /= total;
